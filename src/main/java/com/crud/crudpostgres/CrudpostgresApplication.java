d1bf8dd28cf5adecc136ad43019f7bfee1428057
Mac_Developer <araozangel842@gmail.com>
2023-03-06 21:06:42 +0200

    Starting project

---
+++ src/main/java/com/crud/crudpostgres/CrudpostgresApplication.java
@@ -0,0 +1,13 @@
+package com.crud.crudpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudpostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudpostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crud/crudpostgres/CrudpostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.crudpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudpostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
