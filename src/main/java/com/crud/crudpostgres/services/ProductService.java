8605366daf24e0d844770da2251b61e744bbaef7
Mac_Developer <araozangel842@gmail.com>
2023-03-07 13:48:52 +0200

    Add post method

--- src/main/java/com/crud/crudpostgres/services/ProductService.java
+++ src/main/java/com/crud/crudpostgres/services/ProductService.java
@@ -1,8 +1,12 @@
 package com.crud.crudpostgres.services;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.crud.crudpostgres.interfaces.ProductRepository;
@@ -16,5 +20,21 @@ public class ProductService {
   
   public List<Product> getProducts() {
     return productRepository.findAll();
+  }
+
+  public ResponseEntity<Product> saveProduct(Product product) {
+    Optional<Product> exist = productRepository.findByName(product.getName());
+    HashMap<String, Object> res = new HashMap<>();
+
+
+    if (exist.isPresent() && product.getId() != exist.get().getId()) {
+      res.put("error", true);
+      res.put("message", "Product already exists");
+      return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+    productRepository.save(product);
+    res.put("data", product);
+    res.put("message", "Product created successfully");
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
   }
 }
