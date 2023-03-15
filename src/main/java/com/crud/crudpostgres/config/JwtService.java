62496844f0522334daf6153d74785f6a20e0428a
Mac_Developer <araozangel842@gmail.com>
2023-03-15 20:14:00 +0200

    Add method to validate token

--- src/main/java/com/crud/crudpostgres/config/JwtService.java
+++ src/main/java/com/crud/crudpostgres/config/JwtService.java
@@ -45,6 +45,19 @@ public class JwtService {
     .compact();
   }
 
+  public Boolean validateToken(String token, UserDetails userDetails) {
+    final String username = extractUserName(token);
+    return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
+  }
+
   private Claims extractAllClaims(String jwt) {
     return Jwts
     .parserBuilder()
