c2f0adfb965c5fb53302e46ec12c472a73331171
Mac_Developer <araozangel842@gmail.com>
2023-03-15 08:30:20 +0200

    Create method to generate a token

--- src/main/java/com/crud/crudpostgres/config/JwtService.java
+++ src/main/java/com/crud/crudpostgres/config/JwtService.java
@@ -1,10 +1,15 @@
 package com.crud.crudpostgres.config;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -19,6 +24,20 @@ public class JwtService {
   public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
     final Claims claims = extractAllClaims(token);
     return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(
+    Map<String, Object> extraClaims,
+    UserDetails userDetails
+  ){
+    return Jwts
+    .builder()
+    .setClaims(extraClaims)
+    .setSubject(userDetails.getUsername())
+    .setIssuedAt(new Date(System.currentTimeMillis()))
+    .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+    .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+    .compact();
   }
 
   private Claims extractAllClaims(String jwt) {
